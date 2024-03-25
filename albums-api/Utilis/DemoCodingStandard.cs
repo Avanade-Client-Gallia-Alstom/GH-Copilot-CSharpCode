15010f72b96b33f92b1b48d17e0fcdc743305fbc
Supriya Sumit Vispute <s.sumit.vispute@accenture.com>
2024-03-25 16:50:59 +0530

    Commit all

---
+++ albums-api/Utilis/DemoCodingStandard.cs
@@ -0,0 +1,8 @@
+﻿namespace albums_api.Utilis
+{
+    public class DemoCodingStandard
+    {
+        //Create a function following coding standard file #file:'CodingStandard.json'  to check if a string is a palindrome 
+
+    }
+}
